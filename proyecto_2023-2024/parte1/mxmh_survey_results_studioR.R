6a11b2bf194b306da14730b08e718ff9e39b9326
Victor Sanz <vichisanz99@gmail.com>
2024-05-13 18:55:05 +0200

    update IA-BD_PC
    ;18:55;13.05.2024

--- proyecto_2023-2024/parte1/mxmh_survey_results_studioR.R
+++ proyecto_2023-2024/parte1/mxmh_survey_results_studioR.R
@@ -1,6 +1,27 @@
 # fecha: 10-05-2024
 # nombre: Victor Sanz Arevalo
 
+# Posibles objetivos:
+"""
+1. Analisis de preferencias musicales y bienestar mental:
+Realizar un analisis exploratorio de las variables relacionadas
+con las preferencias musicales y el bienestar mental de los 
+usuarios, como la frequencia de escuchar diferentes generos 
+musicales y los respectivos niveles (0-10) de ansiedad, 
+depresion, insomnio y TOC. ?Relacion entre ciertos generos
+musicales y los niveles de bienestar mental
+
+2. Efectos de la musica en el bienestar mental: 
+Investigar si ay alguna relacion entre la percepcion de los efectos de la 
+musica en el bienestar mental y los niveles reales de ansiedad, 
+depresion, insomnio y TOC reportados por los encuestados.
+
+3. Prediccion de la cantidad de tiempo dedicado a escuchar musica:
+Utilizar variables como la edad, el genero musical favorito y otras 
+caracteristicas demograficas. Construir un modelo para predecir cuanto tiempo 
+dedica una persona a escuchar musica diariamente.
+"""
+
 # Cargar el dataset:
 mxmh_survey_results <- read.csv(file.choose(), header = TRUE, sep = ",")
 
